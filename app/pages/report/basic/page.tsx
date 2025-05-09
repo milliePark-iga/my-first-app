fb913da08ce5d817c5608e5feb06f745a8d063c5
milliePark-iga <millie.park@igaworks.com>
2025-05-09 10:12:18 +0900

    [feat]페이지 추가

---
+++ app/pages/report/basic/page.tsx
@@ -0,0 +1,9 @@
+function BasicReportPage() {
+  return (
+    <div>
+      <h1>Basic Report</h1>
+      <p>This is the basic report page.</p>
+    </div>
+  );
+}
+export default BasicReportPage;
---
+++ app/pages/report/page.tsx
@@ -0,0 +1,9 @@
+function ReportPage() {
+  return (
+    <div>
+      <h1>Report Page</h1>
+      <p>This is the report page.</p>
+    </div>
+  );
+}
+export default ReportPage;
